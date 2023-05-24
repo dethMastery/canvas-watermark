0df4ed78b7baeb21152c91550530b89133c4ef9f
dethMastery <me@suphakit.net>
2023-05-24 12:48:59 +0700

    chore: title options

--- bin/cli.js
+++ bin/cli.js
@@ -4,22 +4,27 @@ const yargs = require('yargs')
 const wtm = require('./index')
 
 const opt = yargs
-    .usage(
-        "\x1b[32mUsage: 'cMark' or 'cMark -c <Your Custom Config>'\x1b[0m"
-    )
-    .option("config", {
-        alias: 'c',
-        describe: "Add Watermark config",
-        type: "string",
-        demandOption: false
-    })
-    .argv;
+  .usage("\x1b[32mUsage: 'cMark' or 'cMark -c <Your Custom Config>'\x1b[0m")
+  .option('config', {
+    alias: 'c',
+    describe: 'Add Watermark config',
+    type: 'string',
+    demandOption: false,
+  })
+  .option('title', {
+    alias: 't',
+    describe: 'Gallery Title',
+    type: 'string',
+    demandOption: true
+  })
+  .argv
 
-const path = opt._[0];
+const path = opt._[0]
 const config = opt.c
+const title = opt.t
 
 if (path != undefined) {
-  wtm(path, config)
+  wtm(path, config, title)
 } else {
-  console.log('\x1b[31m%s\x1b[0m', 'System Error: invalid folder path');
+  console.log('\x1b[31m%s\x1b[0m', 'System Error: invalid folder path')
 }
