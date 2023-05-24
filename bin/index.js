75ed67bb9e55cef72d361a7a411cbda485e566c5
dethMastery <me@suphakit.net>
2023-05-24 12:49:52 +0700

    chore: camera model

--- bin/index.js
+++ bin/index.js
@@ -1,12 +1,13 @@
 const fs = require('fs')
 const sharp = require('sharp')
+const ExifParser = require('exif-parser')
 
 const lists = require('./ignored.json').ignored
 
-function mark(folderPath, config) {
+function mark(folderPath, config, title) {
   // const redBan = '\x1b[31m%s\x1b[0m'
 
-  const pattern = /\.(jpg|png|jpeg)$/
+  const pattern = /\.(jpg|png|jpeg)$/i
 
   function canvasFooter(imgPath) {}
 
@@ -19,9 +20,19 @@ function mark(folderPath, config) {
           .metadata()
           .then(function (metadata) {
             const imgResolution = [metadata.width, metadata.height]
-            console.log('Image width:', imgResolution[0])
-            console.log('Image height:', imgResolution[1])
-            console.log('');
+
+            try {
+              const data = fs.readFileSync(path)
+              const parser = ExifParser.create(data)
+              const result = parser.parse()
+              const model = [result.tags.Model, result.tags.LensModel, new Date(result.tags.DateTimeOriginal * 1000).toLocaleDateString() + ' ' + title]
+
+              console.log(imgResolution);
+              console.log(model);
+              console.log('');
+            } catch (error) {
+              console.log('Error reading or parsing the image file:', error)
+            }
           })
           .catch(function (error) {
             console.log('Error loading the image:', error)
