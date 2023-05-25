5af4b1d348a5e7443f751e61d715baf193d7916c
dethMastery <me@suphakit.net>
2023-05-25 19:34:29 +0700

    chore: default config

---
+++ bin/canvasConfig.js
@@ -0,0 +1,27 @@
+const canvasConfig = {
+  colors: {
+    backgroundColor: '#2e2f2f',
+    titleColor: 'whitesmoke',
+    modelColor: 'whitesmoke',
+    settingColor: 'whitesmoke',
+    watermarkColors: {
+      line1: 'whitesmoke',
+      line2: '#fe6e6f',
+    },
+  },
+  fonts: {
+    titleFont: 'Sarabun',
+    modelFont: 'Sarabun',
+    settingsFont: 'Sarabun',
+    waterMarkFonts: {
+      line1: 'Sarabun',
+      line2: 'Sarabun',
+    },
+  },
+  watermark: {
+    line1: 'dethz',
+    line2: '2018',
+  },
+}
+
+module.exports = canvasConfig
